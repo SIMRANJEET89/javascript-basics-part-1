e1eb54380c56455874d3c91bf10b740a2d7d5cfc
simranjeet kour <simranjeetkour8407@gmail.com>
2024-11-08 01:43:53 -0800

    about date and tym

--- datetym.js
+++ datetym.js
@@ -1,1 +1,29 @@
-console.log("simran")
+//Dates
+
+let myDate = new Date()
+/*console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date (2024, 11, 8)*/
+
+//let myCreatedDate = new Date(2024, 0, 23, 5, 3)
+//let myCreatedDate = new Date ("2024-01-14")
+let myCreatedDate = new Date("01-14-2024")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+
+//console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
